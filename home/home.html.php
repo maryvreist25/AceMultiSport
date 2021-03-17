c04a0f6c552b8b7c990b332938e000cc752f2067
maryvreist25 <50689686+maryvreist25@users.noreply.github.com>
2021-03-16 23:05:39 -0700

    Add files via upload

---
+++ home/home.html.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<!-- Ace Multisport
+file: home.html.php
+Programmer:  Mary Reist
+Date 3/10/21
+-->
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <link href="css/reset.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <link href="css/style.less" rel="stylesheet" type="text/css">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
+    <script>https://cdnjs.cloudflare.com/ajax/libs/javascript.util/0.12.12/javascript.util.min.js</script>
+
+    <title>Ace in the Hole Multisport Events Weekend Assets</title>
+
+</head>
+
+<body>
+
+<div id="container">
+
+    <?php include 'header/header.html.php'; ?>
+
+    <main>
+
+        <h2>ABOUT ACE IN THE HOLE MULTISPORT EVENTS</h2>
+        <p>Ace in the Hole Multisport Events is proud to offer running and triathlon events to athletes of all shapes and sizes,
+            national origins, gender identifications and cultural backgrounds. We offer Events for Every Body.</p>
+
+        <h3>ABOUT THE EVENT</h3>
+        <p>The Annual Ace in the Hole Multisport Weekend is a legendary event in the Oregon triathlon and running community.
+            It has become a traditional destination race for athletes from across the nation.</p>
+        <p>There is something for every level of athletic ability. The weekend includes a first timer triathlon, a sprint, Olympic,
+            and Half-Iron triathlons and 10K and Half marathon runs. Come to experience your first race or come to compete to win,
+            but make sure you come to have fun!</p>
+
+
+        <h2>Starting Times</h2>
+        <h3>Saturday</h3>
+        <p><strong>Long Course Triathlon</strong><br />7:00 AM <br />
+            <strong>Olympic Triathlon</strong><br />7:30 AM <br />
+            <strong>10K</strong><br />7:15 AM <br />
+            <strong>Half Marathon</strong><br />7:15 AM</p>
+
+        <h3><br />Sunday</h3>
+        <p><strong>Sprint Triathlon</strong><br />8:00 AM<br />
+            <strong>Try-a-Tri</strong><br />8:20 AM<br />
+            <strong>Splash n Dash</strong><br />12:00 PM</p>
+
+        <h3 class="item1">What to Bring</h3>
+        <p>Watch the weather closely. The show goes on no matter what the weather is doing.</p>
+        <p>You must wear your chip timing piece during the entire event. You will be given a band that will hold your chip
+            timing piece around your ankle throughout the entire event. Be sure that it is snapped tightly. Be sure to
+            have your Chip Timing piece on before you start the race and be sure to step over the timing mats after each
+            segment of the race. If you are wearing a wet-suit, make sure the timing piece goes under your wetsuit otherwise,
+            you will have a very difficult time getting off your wet suit.</p>
+
+        <p><strong>Swim:</strong> Wetsuits are optional for the swim but will provide buoyancy and warmth. However, many
+            people opt for no wetsuit for a triathlon so no worries. We will provide you with a swim cap but you will want
+            to bring your own goggles.</p>
+
+        <p><strong>Bike:</strong> A biking helmet is mandatory. You will also receive 2 stickers in your package with your
+            race number on them. The small sticker should go on the front of your helmet. The bigger sticker will wrap around
+            your bike frame. Road or mountain bikes are acceptable. For safety reasons, be sure to have end-caps on the end
+            of your handle-bars.</p>
+
+        <p><strong>Run:</strong> You must finish the race with your bib number on the front of you. Some people choose to
+            pin it on at the beginning of the race and have it on for the whole event so they don't have to worry about it.
+            Others pin it on a singlet that they put on once they finish the swim before they head out for the bike. Others
+            use an elastic racing strap that they pin their bib number to and then quickly strap it on before they leave
+            for the run. Use whatever option feels best for you.</p>
+
+        <p>Remember to bring a change of clothing so you can enjoy the post-event festivities.</p>
+
+        <h3 class="item2">FAQs</h3>
+        <p><strong>What are the Rules?</strong> We currently adhere to the USAT Rules for Triathlon &amp; Duathlon.
+            Important rules include no drafting, you must wear a helmet and music is ABSOLUTELY NOT allowed during the
+            bike for obvious safety reasons.</p>
+
+        <p><strong>Can I use a personal music device while cycling?</strong> Absolutely no music devices are allowed
+            during the bike segment for obvious safety risks and will result in immediate disqualification.</p>
+
+        <p><strong>Can I use a personal music device while running?</strong> During any running segments, we prefer for
+            athletes to NOT use music devices to maximize safety, assure a fair competitive environment and assure athletes
+            can hear all course marshal instructions. With that said, we also understand that many athletes rely on music
+            to help endure the challenge of running. To accommodate the needs of our athletes while still assuring maximum
+            safety and a fair, competitive environment, here is the arrangement that we can accommodate.</p>
+
+        <ul>
+            <li>If you chose to use a personal music device, you must always keep one ear open for instructions so only
+                one ear-bud can be used at any time. In addition, if you opt to use music, you will not be eligible for
+                awards, points and rankings. So as an athlete, you have to make a choice - music or awards/points/rankings.</li>
+
+            <li>If you are using a personal music devise with two earbuds, you will be immediately disqualified. Trust us,
+                we don't want to have to enforce a disqualification but in order to assure the safety of all athletes,
+                this is a very important rule.</li>
+
+            <li>We also request that if you chose to use a personal music device that you keep the volume to a minimum to
+                assure all instructions can be heard.</li>
+
+        </ul>
+        <p><strong>Do I need to wear a wetsuit?</strong> No, you do not need to wear a wetsuit. Many will not wear a wetsuit
+            while others will choose to wear a wetsuit because of the buoyancy and warmth factor.</p>
+
+        <p><strong>Do I have to use a road or racing bike?</strong> No. We welcome any type of bike as long as it is
+            functioning properly with brakes and endcaps at the end of your handlebars.</p>
+
+        <footer>
+            <div id="footer">Copyright &copy; Ace in the Hole Multisport Weekend Events<br>
+                <a href="mailto:mary@reist.com">Mary@reist.com</a>
+            </div>
+        </footer>
+
+    </main>
+
+</div>
+
+<script src="css/nav_javasc.js"></script
+
+</body>
+
+</html>
